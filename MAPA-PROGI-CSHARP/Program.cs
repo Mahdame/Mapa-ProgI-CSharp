19847dd4ff01dd08089b4ec4f487a14964403a1c
Maira M Barbosa <mairamourabarbosa@gmail.com>
2020-08-16 14:30:11 -0300

    Adicionada validações de campos vazios

--- MAPA-PROGI-CSHARP/Program.cs
+++ MAPA-PROGI-CSHARP/Program.cs
@@ -4,15 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace MAPA_PROGI_CSHARP
+
+    //Autora: Maira Moura Barbosa
+    //Matricula: 1906020-5
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            //Criando os objetos a serem utilizados
             Presidente presidente = new Presidente();
             Secretaria secretaria = new Secretaria();
             Vendedor vendedor = new Vendedor();
             Cliente cliente;
+
+            //Criando as listas a serem utilizadas
             List<Presidente> presidentes = new List<Presidente>();
             List<Secretaria> secretarias = new List<Secretaria>();
             List<Vendedor> vendedores = new List<Vendedor>();
@@ -41,49 +47,152 @@ namespace MAPA_PROGI_CSHARP
                     case 1:
                         Console.WriteLine("\n*** Cadastrar Presidente ***");
                         presidente.CadastrarFuncionario();
-                        presidentes.Add(presidente);
+
+                        if (presidente.Nome == "" || presidente.Documento == "")
+                        {
+                            Console.WriteLine("O cadastro não pode ser vazio.");
+                        }
+                        else
+                        {
+                            presidentes.Add(presidente);
+                        }
+
                         break;
                     case 2:
                         Console.WriteLine("\n*** Cadastrar Secretaria ***");
                         secretaria.CadastrarFuncionario();
-                        secretarias.Add(secretaria);
+
+                        if (secretaria.Nome == "" || secretaria.Documento == "")
+                        {
+                            Console.WriteLine("O cadastro não pode ser vazio.");
+                        }
+                        else
+                        {
+                            secretarias.Add(secretaria);
+                        }
+
                         break;
                     case 3:
                         Console.WriteLine("\n*** Cadastrar Vendedor ***");
                         vendedor.CadastrarFuncionario();
-                        vendedores.Add(vendedor);
+
+                        if (vendedor.Nome == "" || vendedor.Documento == "")
+                        {
+                            Console.WriteLine("O cadastro não pode ser vazio.");
+                        }
+                        else
+                        {
+                            vendedores.Add(vendedor); ;
+                        }
+
                         break;
                     case 4:
                         Console.WriteLine("\n*** Cadastrar Cliente ***");
                         cliente = new Cliente();
                         cliente.CadastrarCliente();
-                        clientes.Add(cliente);
+
+                        if (cliente.Nome == "" || cliente.Documento == "")
+                        {
+                            Console.WriteLine("O cadastro não pode ser vazio.");
+                        }
+                        else
+                        {
+                            clientes.Add(cliente);
+                        }
+
                         break;
                     case 5:
                         Console.WriteLine("\n*** Comissao dos Vendedores ***");
                         string comissao = Console.ReadLine();
                         vendedor.Comissao = Convert.ToDouble(comissao);
-                        Console.WriteLine($"Comissao definida: {comissao}.");
+                        Console.WriteLine($"Comissao definida: R$ {comissao}.");
+
                         break;
                     case 6:
                         Console.WriteLine("\n*** Calcula Salario Anual ***");
-                        Console.WriteLine($"Calcular Salario Anual de {presidente.Nome = "Asdrubal"}");
-                        Console.Write($"Salario: ");
-                        string salarioPresidente = Console.ReadLine();
-                        var anualPres = presidente.CalculaSalarioAnual(Convert.ToDouble(salarioPresidente));
-                        Console.WriteLine($"Salario Anual: " + anualPres.ToString());
+                        Console.WriteLine("Escolha o cargo do funcionario: ");
+                        Console.WriteLine("1 - Presidente");
+                        Console.WriteLine("2 - Secretaria");
+                        Console.WriteLine("3 - Vendedor");
+                        string input2 = Console.ReadLine();
 
-                        Console.WriteLine($"Calcular Salario Anual de {secretaria.Nome = "Ana"}");
-                        Console.WriteLine($"Salario: ");
-                        string salarioSecretaria = Console.ReadLine();
-                        var anualSec = secretaria.CalculaSalarioAnual(Convert.ToDouble(salarioSecretaria));
-                        Console.WriteLine($"Salario Anual: " + anualSec.ToString());
+                        int opcao2 = 9;
+                        opcao2 = Convert.ToInt32(input2);
+                        string nomeFuncionario;
+                        string salario;
+
+                        switch (opcao2)
+                        {
+                            case 1:
+                                Console.Write("Nome: ");
+                                nomeFuncionario = Console.ReadLine();
+
+                                //Pesquisa o nome na lista de presidentes
+                                var pesquisaPresidente = presidentes.Where(x => x.Nome == nomeFuncionario).FirstOrDefault();
+
+                                if (pesquisaPresidente == null)
+                                {
+                                    Console.WriteLine("===> ERRO: Nao ha cliente cadastrado com esse nome. <===");
+                                    break;
+                                }
+
+                                Console.Write("Salario Mensal: ");
+                                salario = Console.ReadLine();
+                                var anualPres = presidente.CalculaSalarioAnual(Convert.ToDouble(salario));
+                                Console.WriteLine($"Salario Anual: R$ " + anualPres.ToString());
+
+                                break;
+                            case 2:
+                                Console.Write("Nome: ");
+                                nomeFuncionario = Console.ReadLine();
+
+                                //Pesquisa o nome na lista de secretarias
+                                var pesquisaSecretaria = secretarias.Where(x => x.Nome == nomeFuncionario).FirstOrDefault();
+
+                                if (pesquisaSecretaria == null)
+                                {
+                                    Console.WriteLine("===> ERRO: Nao ha cliente cadastrado com esse nome. <===");
+                                    break;
+                                }
+
+                                Console.Write("Salario Mensal: ");
+                                salario = Console.ReadLine();
+                                var anualSec = presidente.CalculaSalarioAnual(Convert.ToDouble(salario));
+                                Console.WriteLine($"Salario Anual: R$ " + anualSec.ToString());
+
+                                break;
+                            case 3:
+                                Console.Write("Nome: ");
+                                nomeFuncionario = Console.ReadLine();
+
+                                //Pesquisa o nome na lista de vendedores
+                                var pesquisaVendedor = vendedores.Where(x => x.Nome == nomeFuncionario).FirstOrDefault();
+
+                                if (pesquisaVendedor == null)
+                                {
+                                    Console.WriteLine("===> ERRO: Nao ha cliente cadastrado com esse nome. <===");
+                                    break;
+                                }
+
+                                Console.Write("Salario Mensal: ");
+                                salario = Console.ReadLine();
+                                var anualVend = presidente.CalculaSalarioAnual(Convert.ToDouble(salario));
+                                Console.WriteLine($"Salario Anual: R$ " + anualVend.ToString());
+
+                                break;
+
+                            default:
+                                Console.WriteLine("Opcao invalida!");
+                                break;
+                        }
                         break;
                     case 7:
                         Console.WriteLine("*** Verificar Senha ***");
                         Console.WriteLine($"Verificar se a senha do cliente e 123456, segredo ou teste.\n");
                         Console.Write("Digite o nome do usuario: ");
                         var nome = Console.ReadLine();
+
+                        //Pesquisa o nome na lista de clientes
                         var pesquisaCliente = clientes.Where(x => x.Nome == nome).FirstOrDefault();
 
                         if (pesquisaCliente == null)
@@ -101,23 +210,32 @@ namespace MAPA_PROGI_CSHARP
 
                         break;
                     case 8:
-                        Console.WriteLine("\n*** Lista de Pessoas Cadastradas ***");
-                        presidente.presidentes = presidentes;
+                        Console.WriteLine("\n*** Lista de Pessoas Cadastradas ***");                        
+
+                        if (presidente.Nome == null || secretaria.Nome == null || vendedor.Nome == null)
+                        {
+                            Console.WriteLine("===> ERRO: Nao ha registros. <==="); ;
+                        }
+
+                        presidentes.ForEach(x => Console.WriteLine("\n==> Presidentes <==\n" + "Nome: " + x.Nome + "\n" + "Documento: " + x.Documento + "\n"));
                         presidente.ExibeDados();
 
-                        secretaria.secretarias = secretarias;
+                        secretarias.ForEach(y => Console.WriteLine("==> Secretarias <==\n" + "Nome: " + y.Nome + "\n" + "Documento: " + y.Documento + "\n"));
                         secretaria.ExibeDados();
 
-                        vendedor.vendedores = vendedores;
+                        vendedores.ForEach(w => Console.WriteLine("==> Vendedores <==\n" + "Nome: " + w.Nome + "\n" + "Documento: " + w.Documento + "\n"));
                         vendedor.ExibeDados();
 
-                        clientes.ForEach(x => Console.WriteLine("\n==> Clientes <==\n" + "Nome: " + x.Nome));
+                        clientes.ForEach(z => Console.WriteLine("==> Clientes <==\n" + "Nome: " + z.Nome + "\n" + "Documento: " + "\n"));
+
                         break;
                     case 0:
                         Console.WriteLine("Saindo...");
+
                         break;
                     default:
                         Console.WriteLine("Opcao invalida!");
+
                         break;
                 }
             }
